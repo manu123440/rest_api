483a3bed3f3287332928e9cf9fb39d6ed2b8112a
manu123440 <55475898+manu123440@users.noreply.github.com>
2023-03-19 20:33:07 +0530

    Update checkSubscription.js

--- routes/checkSubscription.js
+++ routes/checkSubscription.js
@@ -8,32 +8,71 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const Stripe = require("./stripe");
 
+let selectFunction = (item) => {
+  let options = {
+    method: "POST",
+    url: "https://itslocaltv.io/api/select.php",
+    formData: {
+      select_query: item,
+    },
+  };
+  return options;
+};
+
 router.post('/checkSubscription',async (req, res, next) => {
-	const sub_id = req.body.sub_id;
+	const email = req.body.email;
+
+	let opt1 = selectFunction(
+	    "select session_id from users where email = '"
+	    .concat(`${email}`)
+	    .concat("'")
+	);
+	
 	let isActive = false;
 
 	try {
-		const subscription = await Stripe.retrieveSubscription(sub_id);
-		// console.log(subscription);
-		if(subscription.status === 'active' || subscription.status === 'trialing') {
-		 	isActive = true;
-		 	return res.json({
-		 		isActive
-			})
-		}
-		else {
-		 	isActive = false;
-		  return res.json({
-		  	isActive
-			})
-		}
+		request(opt1, async (error, response) => {
+      		if (error) throw new Error(error);
+      		else {
+        		let z = JSON.parse(response.body);
+
+        		if(z !== null && z[0] !== undefined) {
+        			const session_id = z[0].session_id;
+        			const session = await stripe.checkout.sessions.retrieve(session_id);
+        			const sub_id = session.subscription;
+        			// console.log(sub_id);
+
+        			const subscription = await Stripe.retrieveSubscription(sub_id);
+					// console.log(subscription);
+					if(subscription.status === 'active' || subscription.status === 'trialing') {
+					 	isActive = true;
+					 	return res.json({
+					 		isActive
+						})
+					}
+					else {
+					 	isActive = false;
+					  return res.json({
+					  	isActive
+						})
+					}
+        		}
+        		else {
+					isActive = false;
+					return res.json({
+					  	isActive
+					})
+				}
+        	}
+        })
 	}
 	catch(err) {
 		isActive = false;
-	  return res.json({
+	  	return res.json({
 		  	isActive
 		})
 	}
+
 })
 
 module.exports = router;
