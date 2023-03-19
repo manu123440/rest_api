f9fceca35f9bcda642843433eb43788f205ca634
manu123440 <55475898+manu123440@users.noreply.github.com>
2023-03-19 22:21:49 +0530

    Update cancelSubscription.js

--- routes/cancelSubscription.js
+++ routes/cancelSubscription.js
@@ -8,34 +8,72 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const Stripe = require("./stripe");
 
+let selectFunction = (item) => {
+  let options = {
+    method: "POST",
+    url: "https://itslocaltv.io/api/select.php",
+    formData: {
+      select_query: item,
+    },
+  };
+  return options;
+};
+
 router.post('/cancelSubscription', async (req, res, next) => {
-	const sub_id = req.body.sub_id;
+	const email = req.body.email;
+
+	let opt1 = selectFunction(
+	    "select session_id from users where email = '"
+	    .concat(`${email}`)
+	    .concat("'")
+	);
 	let isCanceled = false;
 
 	try {
-		const subscription = await Stripe.retrieveSubscription(sub_id);
+		request(opt1, async (error, response) => {
+      if (error) throw new Error(error);
+      else {
+       	let z = JSON.parse(response.body);
 
-		if(subscription.status !== 'canceled') {
-			const cancelSub = await Stripe.cancelSubscription(sub_id);
-			if(cancelSub.status === 'canceled') {
-				isCanceled = true;
-				return res.json({
-					isCanceled
-				})
-			}
-			else {
-				isCanceled = false;
-				return res.json({
-					isCanceled
-				})
-			}
-		}
-		else {
-			isCanceled = false;
-			return res.json({
-				isCanceled
-			})
-		}
+       	if(z !== null && z[0] !== undefined) {
+       		const session_id = z[0].session_id;
+       		const session = await stripe.checkout.sessions.retrieve(session_id);
+       		const sub_id = session.subscription;
+
+       		if (sub_id !== null) {
+	       		const subscription = await Stripe.retrieveSubscription(sub_id);
+
+						if(subscription.status !== 'canceled') {
+							const cancelSub = await Stripe.cancelSubscription(sub_id);
+							if(cancelSub.status === 'canceled') {
+								isCanceled = true;
+								return res.json({
+									isCanceled
+								})
+							}
+							else {
+								isCanceled = false;
+								return res.json({
+									isCanceled
+								})
+							}
+						}
+					}
+					else {
+						isCanceled = false;
+						return res.json({
+							isCanceled
+						})
+					}
+       	}
+       	else {
+					isCanceled = false;
+					return res.json({
+						isCanceled
+					})
+				}
+      }
+    })
 	}
 	catch(err) {
 		isCanceled = false;
